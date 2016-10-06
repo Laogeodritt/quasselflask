3593b28c70b622cbfc367932862eb383ded3ead4
Marc-Alexandre Chan <marcalexc@arenthil.net>
2016-10-05 20:31:25 -0400

    Bump version to 0.3 - I think this is overdue!

--- quasselflask/__init__.py
+++ quasselflask/__init__.py
@@ -8,7 +8,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-__version__ = "0.1"
+__version__ = "0.3"
 
 app = Flask(__name__, instance_path=os.environ.get('QF_CONFIG_PATH', None), instance_relative_config=True)
 db = SQLAlchemy(app)
